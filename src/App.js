f30f7a13a0cbc890ba8f4ab7ee6db40d84b5f80b
aleverarise <aleverarise@gmail.com>
2018-04-15 17:32:48 -0400

    update files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import Map from './components/Map';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			markers: [{"id":1,"name":"Mexico","lat":19.3202829,"lng":-99.4328154,"icon":"https://cdn1.iconfinder.com/data/icons/ensign-11/512/168_Ensign_Flag_Nation_mexico-20.png"},{"id":2,"name":"España","lat":40.4378698,"lng":-3.8196223,"icon":"https://cdn3.iconfinder.com/data/icons/flags-of-countries-3/128/Spain-20.png"},{"id":3,"name":"Chile","lat":-33.4724727,"lng":-70.9100268,"icon":"https://cdn1.iconfinder.com/data/icons/ensign-11/512/56_Ensign_Flag_Nation_chile-20.png"},{"id":4,"name":"Peru","lat":-9.1303223,"lng":-84.0734247,"icon":"https://cdn1.iconfinder.com/data/icons/ensign-11/512/201_Ensign_Flag_Nation_peru-20.png"},{"id":5,"name":"Argentina","lat":-37.0514278,"lng":-81.6619065,"icon":"https://cdn1.iconfinder.com/data/icons/ensign-11/512/16_Ensign_Flag_Nation_Argentina-20.png"}]
+		}
+	}
+	
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Project Maps</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+			<div className="container">
+				<div className="row" >
+					<div className="col s12" >
+						<div className="card" >
+							<div className="card-content">
+								<Map 
+									ref="map"
+									markers={this.state.markers}
+									centerMapCoordinates={[-1.635265, -80.9306805]}
+								/>
+
+								<div className="text-center" >
+									<button className="waves-effect waves-light btn" >
+										<i className="material-icons">keyboard_arrow_left</i>
+									</button>
+
+									<button className="waves-effect waves-light btn" >
+										<i className="material-icons">play_arrow</i>
+									</button>
+									
+									<button className="waves-effect waves-light btn" >
+										<i className="material-icons">stop</i>
+									</button>
+
+									<button className="waves-effect waves-light btn" >
+										<i className="material-icons">keyboard_arrow_right</i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
---
+++ src/components/Map.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps';
+
+const google = window.google;
+
+class Map extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            markers: this.props.markers,
+            interval: '',
+            actualPosition: 0,
+            currentLat: this.props.centerMapCoordinates[0],
+            currentLng: this.props.centerMapCoordinates[1]
+        }
+    }
+
+    componentDidMount() {
+        let interval = setInterval(this.timer, 3000);
+        this.setState({ interval });
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.state.interval);
+    }
+
+    timer = () => {
+        const { markers, actualPosition } = this.state
+        this.setState({
+            actualPosition: actualPosition + 1 < markers.length ? actualPosition + 1 : 0,
+            currentLat: markers[actualPosition].lat,
+            currentLng: markers[actualPosition].lng
+        })
+    }
+    
+    render() {
+        const { markers } = this.state;
+
+        let dynamicMarkers;
+        if(markers){
+            dynamicMarkers = markers.map( (value, index) => {
+                return (
+                    <Marker 
+                        key={index}
+                        position={ { lat: value.lat, lng: value.lng } } 
+                        defaultTitle={ value.name }
+                        icon={ value.icon }
+                    />
+                )
+            })
+        }
+
+        const MyMapComponent = withGoogleMap((props) => 
+            <GoogleMap
+                defaultZoom={4}
+                defaultCenter={ { lat: this.state.currentLat, lng: this.state.currentLng} }
+                defaultTitle="Mapa"
+
+            >
+                { dynamicMarkers }
+            </GoogleMap>
+        )
+        return (
+            <MyMapComponent 
+                loadingElement={ <div style={ {height: '100%'} } /> }
+                containerElement={ <div style={{ height: '70vh' }} /> }
+                mapElement={ <div style={{ height: '100%' }} /> }
+            />
+        );
+    }
+}
+
+export default Map;
